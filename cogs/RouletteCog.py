1032f124cb42f7e31118d0e83ac4cdf6ae9ac828
garett322 <49880347+garett322@users.noreply.github.com>
2020-02-27 09:54:40 -0800

    Update RouletteCog.py

--- cogs/RouletteCog.py
+++ cogs/RouletteCog.py
@@ -14,13 +14,19 @@ class RouletteCog(commands.Cog, name = "RouletteCog" ):
         
     @commands.command(name = 'roulette' )
     async def russian_roulette(self, ctx):
-        await ctx.send('Please mention the person you want to challenge:')
-        try:
-            msg = await self.bot.wait_for('message', timeout=10.0)
-        except asyncio.TimeoutError:
-            await ctx.send('You took too long... Game has been cancelled')
-        else:
-            await ctx.send('You said {0.content}, {0.author}.'.format(msg))
+		opponent_id = ctx.mentions[0].id
+		oponnent_user = self.bot.get_user(oponnent_id)
+		if (!opponent_user):
+			await ctx.send('Please mention the person you want to challenge:')
+		else:
+			await ctx.send(opponent_user.mention + ' Roulette Works!')
+			#try:
+			#	msg = await self.bot.wait_for('message', timeout=15.0)
+			#except asyncio.TimeoutError:
+			#	await ctx.send('You took too long... Game has been cancelled')
+			#else:
+			#	await ctx.send('You said {0.content}, {0.author}.'.format(msg))
+			
                            
 def setup(bot):
     bot.add_cog(RouletteCog(bot))
