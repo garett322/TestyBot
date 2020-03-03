21525c4c0197710dac409fb0d4bffcdf35452e0c
garett322 <49880347+garett322@users.noreply.github.com>
2020-03-03 08:43:55 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import discord
 from discord.ext import commands
-
 client = commands.Bot(command_prefix = ".", selfbot = 'false')
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-
+	print('Logged in as {0.user}'.format(client))
+	
+@client.event
+async def on_message():
+	if (message.channel.id == '676897947731886085'):
+        pass
+	else:
+		await message.author.send('Please use the bot-commands channel. Thanks!')
 client.load_extension('cogs.TestCog')
 client.load_extension('cogs.RouletteCog')
-
 
 client.run('Njc2ODk3NDE2NjQwOTg3MTQ3.XknFVQ.gZPzOHRgqOMCNkePNIpAP2iRvDk')
