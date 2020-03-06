a09fe14a3c5dbc1305ddae65ccc5c1a02076ebec
garett322 <49880347+garett322@users.noreply.github.com>
2020-03-06 13:02:16 -0800

    Update RouletteCog.py

--- cogs/RouletteCog.py
+++ cogs/RouletteCog.py
@@ -18,127 +18,131 @@ class RouletteCog(commands.Cog, name = "RouletteCog" ):
 		if not ctx.message.mentions:
 			await ctx.send('Please mention a valid user to challenge.')
 			return
-		elif ctx.message.mentions[1].id:
-			await ctx.send('Please only mention 1 user to challenge.')
-			return
 		else:
-			opponent_id = ctx.message.mentions[0].id
-			opponent_user = self.bot.get_user(opponent_id)
-		
-			if opponent_id == ctx.author.id:
-				await ctx.send('You cant challenge yourself ' + opponent_user.mention)
-			elif opponent_id == self.bot.user.id:
-				await ctx.send('You cant challenge me ' + ctx.author.mention + '. Lmao. I would kick your ass anyways.')
+			try:
+				ctx.message.mentions[1].id
+			except IndexError:
+				await ctx.send('Please only mention 1 user to challenge.')
+				return
 			else:
-				await ctx.send('Hey ' + opponent_user.mention + ', ' + ctx.author.mention + ' has challenged you to a game of Russian Roulette! Would you like to accept?')
 				
-				
-				def check(message):
-					return message.author == opponent_user and (message.content.lower() == 'y' or message.content.lower() == 'yes')
-				
-				
-				try:
-					msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-				except asyncio.TimeoutError:
-					await ctx.send(opponent_user.mention + ' took too long to accept... Game has been cancelled')
+				opponent_id = ctx.message.mentions[0].id
+				opponent_user = self.bot.get_user(opponent_id)
+		
+				if opponent_id == ctx.author.id:
+						await ctx.send('You cant challenge yourself ' + opponent_user.mention)
+				elif opponent_id == self.bot.user.id:
+					await ctx.send('You cant challenge me ' + ctx.author.mention + '. Lmao. I would kick your ass anyways.')
 				else:
-					clip = [i for i in range(6)]
-					bullet = choice(clip)
-					await ctx.send(ctx.author.mention + ' slides a bullet into the revolver, closes it,  and spins the cylinder. They then hand the revolver to ' + opponent_user.mention)
-					await ctx.send("say 'shoot' to pull the trigger")
+					await ctx.send('Hey ' + opponent_user.mention + ', ' + ctx.author.mention + ' has challenged you to a game of Russian Roulette! Would you like to accept?')
 					
 					
-					
-										     
 					def check(message):
-						return message.author == opponent_user and (message.content == 'shoot')
-										     
+						return message.author == opponent_user and (message.content.lower() == 'y' or message.content.lower() == 'yes')
+					
+					
 					try:
 						msg = await self.bot.wait_for('message', timeout=15.0, check=check)
 					except asyncio.TimeoutError:
-						await ctx.send(opponent_user.mention + ' took too long to pull the trigger. ' + ctx.author.mention + ' picks up the revolver and shoots ' + opponent_user.mention + ' in the foot.')
-						await ctx.send(ctx.author.mention + ' Wins!!!')
+						await ctx.send(opponent_user.mention + ' took too long to accept... Game has been cancelled')
 					else:
-						if bullet == 0:
-							await ctx.send('BANG! ' + opponent_user.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+						clip = [i for i in range(6)]
+						bullet = choice(clip)
+						await ctx.send(ctx.author.mention + ' slides a bullet into the revolver, closes it,  and spins the cylinder. They then hand the revolver to ' + opponent_user.mention)
+						await ctx.send("say 'shoot' to pull the trigger")
+					
+					
+					
+										     
+						def check(message):
+							return message.author == opponent_user and (message.content == 'shoot')
+											     
+						try:
+							msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+						except asyncio.TimeoutError:
+							await ctx.send(opponent_user.mention + ' took too long to pull the trigger. ' + ctx.author.mention + ' picks up the revolver and shoots ' + opponent_user.mention + ' in the foot.')
 							await ctx.send(ctx.author.mention + ' Wins!!!')
 						else:
-							await ctx.send('Click... ' + opponent_user.mention + ' hands the revolver over to ' + ctx.author.mention)
-							
-							
-							def check(message):
-								return message.author == ctx.author and (message.content == 'shoot')
-
-							try:
-								msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-							except asyncio.TimeoutError:
-								await ctx.send(ctx.author.mention + ' took too long to pull the trigger. ' + opponent_user.mention + ' picks up the revolver and shoots ' + ctx.author.mention + ' in the foot.')
-								await ctx.send(opponent_user.mention + ' Wins!!!')
+							if bullet == 0:
+								await ctx.send('BANG! ' + opponent_user.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+								await ctx.send(ctx.author.mention + ' Wins!!!')
 							else:
-								if bullet == 1:
-									await ctx.send('BANG! ' + ctx.author.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+								await ctx.send('Click... ' + opponent_user.mention + ' hands the revolver over to ' + ctx.author.mention)
+								
+							
+								def check(message):
+									return message.author == ctx.author and (message.content == 'shoot')
+
+								try:
+									msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+								except asyncio.TimeoutError:
+									await ctx.send(ctx.author.mention + ' took too long to pull the trigger. ' + opponent_user.mention + ' picks up the revolver and shoots ' + ctx.author.mention + ' in the foot.')
 									await ctx.send(opponent_user.mention + ' Wins!!!')
 								else:
-									await ctx.send('Click... ' + ctx.author.mention + ' hands the revolver over to ' + opponent_user.mention)
-									
-									
-									def check(message):
-										return message.author == opponent_user and (message.content == 'shoot')
-
-									try:
-										msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-									except asyncio.TimeoutError:
-										await ctx.send(opponent_user.mention + ' took too long to pull the trigger. ' + ctx.author.mention + ' picks up the revolver and shoots ' + opponent_user.mention + ' in the foot.')
-										await ctx.send(ctx.author.mention + ' Wins!!!')
+									if bullet == 1:
+										await ctx.send('BANG! ' + ctx.author.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+										await ctx.send(opponent_user.mention + ' Wins!!!')
 									else:
-										if bullet == 2:
-											await ctx.send('BANG! ' + opponent_user.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+										await ctx.send('Click... ' + ctx.author.mention + ' hands the revolver over to ' + opponent_user.mention)
+									
+									
+										def check(message):
+											return message.author == opponent_user and (message.content == 'shoot')
+	
+										try:
+											msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+										except asyncio.TimeoutError:
+											await ctx.send(opponent_user.mention + ' took too long to pull the trigger. ' + ctx.author.mention + ' picks up the revolver and shoots ' + opponent_user.mention + ' in the foot.')
 											await ctx.send(ctx.author.mention + ' Wins!!!')
 										else:
-											await ctx.send('Click... ' + opponent_user.mention + ' hands the revolver over to ' + ctx.author.mention)
-											
-											
-											def check(message):
-												return message.author == ctx.author and (message.content == 'shoot')
-
-											try:
-												msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-											except asyncio.TimeoutError:
-												await ctx.send(ctx.author.mention + ' took too long to pull the trigger. ' + opponent_user.mention + ' picks up the revolver and shoots ' + ctx.author.mention + ' in the foot.')
-												await ctx.send(opponent_user.mention + ' Wins!!!')
+											if bullet == 2:
+												await ctx.send('BANG! ' + opponent_user.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+												await ctx.send(ctx.author.mention + ' Wins!!!')
 											else:
-												if bullet == 3:
-													await ctx.send('BANG! ' + ctx.author.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+												await ctx.send('Click... ' + opponent_user.mention + ' hands the revolver over to ' + ctx.author.mention)
+											
+												
+												def check(message):
+													return message.author == ctx.author and (message.content == 'shoot')
+
+												try:
+													msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+												except asyncio.TimeoutError:
+													await ctx.send(ctx.author.mention + ' took too long to pull the trigger. ' + opponent_user.mention + ' picks up the revolver and shoots ' + ctx.author.mention + ' in the foot.')
 													await ctx.send(opponent_user.mention + ' Wins!!!')
 												else:
-													await ctx.send('Click... ' + ctx.author.mention + ' hands the revolver over to ' + opponent_user.mention)
-													
-													def check(message):
-														return message.author == opponent_user and (message.content == 'shoot')
-
-													try:
-														msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-													except asyncio.TimeoutError:
-														await ctx.send(opponent_user.mention + ' took too long to pull the trigger. ' + ctx.author.mention + ' picks up the revolver and shoots ' + opponent_user.mention + ' in the foot.')
-														await ctx.send(ctx.author.mention + ' Wins!!!')
+													if bullet == 3:
+														await ctx.send('BANG! ' + ctx.author.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+														await ctx.send(opponent_user.mention + ' Wins!!!')
 													else:
-														if bullet == 4:
-															await ctx.send('BANG! ' + opponent_user.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+														await ctx.send('Click... ' + ctx.author.mention + ' hands the revolver over to ' + opponent_user.mention)
+													
+														def check(message):
+															return message.author == opponent_user and (message.content == 'shoot')
+
+														try:
+															msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+														except asyncio.TimeoutError:
+															await ctx.send(opponent_user.mention + ' took too long to pull the trigger. ' + ctx.author.mention + ' picks up the revolver and shoots ' + opponent_user.mention + ' in the foot.')
 															await ctx.send(ctx.author.mention + ' Wins!!!')
 														else:
-															await ctx.send('Click... ' + opponent_user.mention + ' hands the revolver over to ' + ctx.author.mention)
-															
-															def check(message):
-																return message.author == ctx.author and (message.content == 'shoot')
-
-															try:
-																msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-															except asyncio.TimeoutError:
-																await ctx.send(ctx.author.mention + ' took too long to pull the trigger. ' + opponent_user.mention + ' picks up the revolver and shoots ' + ctx.author.mention + ' in the foot.')
-																await ctx.send(opponent_user.mention + ' Wins!!!')
+															if bullet == 4:
+																await ctx.send('BANG! ' + opponent_user.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+																await ctx.send(ctx.author.mention + ' Wins!!!')
 															else:
-																await ctx.send('BANG! ' + ctx.author.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
-																await ctx.send(opponent_user.mention + ' Wins!!!')
+																await ctx.send('Click... ' + opponent_user.mention + ' hands the revolver over to ' + ctx.author.mention)
+																
+																def check(message):
+																	return message.author == ctx.author and (message.content == 'shoot')
+
+																try:
+																	msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+																except asyncio.TimeoutError:
+																	await ctx.send(ctx.author.mention + ' took too long to pull the trigger. ' + opponent_user.mention + ' picks up the revolver and shoots ' + ctx.author.mention + ' in the foot.')
+																	await ctx.send(opponent_user.mention + ' Wins!!!')
+																else:
+																	await ctx.send('BANG! ' + ctx.author.mention + ' looks down and sees blood slowly seeping through the new hole in their foot...')
+																	await ctx.send(opponent_user.mention + ' Wins!!!')
 						
 						
 						
