27610bbd1e64acf2d1dae431b17c1a1e8453eeef
garett322 <49880347+garett322@users.noreply.github.com>
2020-03-11 09:16:38 -0700

    Update TornCog.py

--- cogs/TornCog.py
+++ cogs/TornCog.py
@@ -5,13 +5,14 @@ from discord.ext import commands
 class TestCog(commands.Cog, name = "TornCog" ):
 	def __init__(self, bot):
 		self.bot = bot
-		
+
 	async def on_message(self, message):
 		if message.author == 'Test Bot#0806':
 			return
-		
+
 	@commands.command(name = 'torn' )
-		async def torny(self, ctx):
-			
+	async def torny(self, ctx):
+		ctx.send('hi')
+
 def setup(bot):
 	bot.add_cog(TornCog(bot))
