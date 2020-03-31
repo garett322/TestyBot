60063fb077ca589ce6bfa5117dbfaf065a12a84c
garett322 <garett322@gmail.com>
2020-03-31 14:19:36 -0700

    a

--- cogs/TornCog.py
+++ cogs/TornCog.py
@@ -36,7 +36,9 @@ class TornCog(commands.Cog, name = "TornCog" ):
 		  await ctx.send(api_key)
 		  await ctx.send(doc['name'])
 		  await ctx.send(doc['api_key'])
-		  #inserted_doc = KEYS.insert_one(doc)
+		  inserted_doc = KEYS.insert_one(doc)
+		  found_doc = KEYS.find_one({"name", playername})
+		  await ctx.send(found_doc)
 		
 		  
 
