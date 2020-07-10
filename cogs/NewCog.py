c8117ea55d85dbae73964b3a0a8254cfee0be805
garett322 <49880347+garett322@users.noreply.github.com>
2020-07-10 09:51:40 -0700

    Update NewCog.py

--- cogs/NewCog.py
+++ cogs/NewCog.py
@@ -1,1 +1,26 @@
+import discord
+from discord.ext import commands
 
+
+class TestCog(commands.Cog, name = "TestCog" ):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	async def on_message(self, message):
+		if message.author == 'Test Bot#0806':
+		return
+		
+	@commands.command(name = 'check' )
+	async def check(self, ctx, *args):
+		if not args:
+			await ctx.send('hello')
+			return
+		else:
+			await ctx.send('Nope. Sorry')
+			return
+      
+        
+
+def setup(bot):
+	bot.add_cog(TestCog(bot))
