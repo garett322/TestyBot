56e6670d18b533d5ecc9bb7acb2cd6b365747f71
garett322 <49880347+garett322@users.noreply.github.com>
2020-07-10 17:47:50 -0700

    Update TornCog.py

--- cogs/TornCog.py
+++ cogs/TornCog.py
@@ -57,7 +57,7 @@ class TornCog(commands.Cog, name = "TornCog" ):
 	@commands.command()
 	async def check(self, ctx, args):
 		for x in args:
-			if (x = "hi"):
+			if x == 'hi':
 				print('Hello')
 
 def setup(bot):
