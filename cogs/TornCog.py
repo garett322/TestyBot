b4953589b5b262fa007836b50f7cc2ef57e3fe25
garett322 <49880347+garett322@users.noreply.github.com>
2020-07-15 16:16:48 -0700

    Update TornCog.py

--- cogs/TornCog.py
+++ cogs/TornCog.py
@@ -63,7 +63,7 @@ class TornCog(commands.Cog, name = "TornCog" ):
 		#with urllib.request.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address=google") as API_data:
 		#	data = json.loads(API_data.read().decode())
 		#	print(data)
-		if args = None
+		if args = None:
 			await ctx.send('no args')
 			return
 			
