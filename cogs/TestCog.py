736f789d0e27ca63f58d463dc1d7a71228dbe974
garett322 <garett322@gmail.com>
2020-12-26 14:19:40 -0800

    p

--- cogs/TestCog.py
+++ cogs/TestCog.py
@@ -3,18 +3,18 @@ from discord.ext import commands
 
 
 class TestCog(commands.Cog, name = "TestCog" ):
-    def __init__(self, bot):
-        self.bot = bot
-
-    async def on_message(self, message):
-        if message.author == 'Test Bot#0806':
-            return
-
-    @commands.command(name = 'test' )
-    async def testy(self, ctx):
-        id_pull = ctx.author.id
-        user = self.bot.get_user(id_pull)
-        await ctx.send(id_pull + ' It Works!')
+	def __init__(self, bot):
+		self.bot = bot
+		
+	
+	async def on_message(self, message):
+		if message.author == 'Test Bot#0806':
+			return
+			
+	@commands.command(name = 'test' )
+		async def testy(self, ctx):
+			await ctx.send('It Works!')
+			return
 
 def setup(bot):
-    bot.add_cog(TestCog(bot))
+	bot.add_cog(TestCog(bot))
