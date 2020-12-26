fd1f7c0dfffe0f31fa3e344b1653d02a9e64fff0
garett322 <garett322@gmail.com>
2020-12-26 13:38:51 -0800

    v

--- cogs/GamblingCog.py
+++ cogs/GamblingCog.py
@@ -9,24 +9,24 @@ db = mclient.API
 KEYS = db.Keys
 
 class GamblingCog(commands.Cog, name = "GamblingCog"):
-  def __init__(self, bot):
-    self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 
-  async def on_message(self, message):
-    if message.author == 'Test Bot#0806':
-      return
+	async def on_message(self, message):
+		if message.author == 'Test Bot#0806':
+			return
 
-  @commands.command(name= 'command1')
-  async def command1(self, ctx):
-    if ctx.guild is None:
-      await ctx.author.send('Please only use this command in DMs ' + ctx.author.mention + '. We dont want everybodyto know your API key.')
-      return
-  else :
-    #inserted_doc = KEYS.insert_one(doc)
-    #found_doc = KEYS.find_one({
-    #"discord_username": str(ctx.author.id)})
-    await ctx.send('works')
-    return
+	@commands.command(name= 'command1')
+	async def command1(self, ctx):
+		if ctx.guild is None:
+			await ctx.author.send('Please only use this command in DMs ' + ctx.author.mention + '. We dont want everybodyto know your API key.')
+			return
+	else :
+		#inserted_doc = KEYS.insert_one(doc)
+		#found_doc = KEYS.find_one({
+		#"discord_username": str(ctx.author.id)})
+		await ctx.send('works')
+		return
 
 def setup(bot):
-  bot.add_cog(TornCog(bot))
+	bot.add_cog(TornCog(bot))
