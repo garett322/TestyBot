9c37647612f3dc3a4ef749890fb9ab596a2f9964
garett322 <garett322@gmail.com>
2020-12-26 21:51:11 -0800

    k

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ async def on_ready():
 	
 @client.event
 async def on_message(message):
-	if (message.channel.id == 676897947731886085) or (message.guild is None):
+	if (message.channel.name == 'bot-commands') or (message.guild is None):
         	pass
 	else:
 		if message.content.startswith('.'):
---
+++ cogs/RoleCog.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+
+
+class RoleCog(commands.Cog, name = "RoleCog" ):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	async def on_message(self, message):
+		if message.author == 'Test Bot#0806':
+			return
+	
+	@commands.command(name = 'role' )
+	async def role(self, ctx, user: discord.member, args):
+		if not args:
+			await ctx.send('No args')
+			return
+		else:
+		 if lower(args) = 'set'
+			await ctx.send('The name of your role:')
+			
+			def check(message):
+				try:
+					msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+				except asyncio.TimeoutError:
+					await ctx.send(ctx.author.mention + 'You took too long to reply. The command has been canceled.')
+				else:
+					for role in ctx.author.roles:
+						if role.name == 'bot kings':
+							ctx.send('dope!!!')
+							return
+						else:
+							ctx.send('NOT DOPE!!!!')
+							return
+
+
+
+
+def setup(bot):
+	bot.add_cog(RoleCog(bot))
