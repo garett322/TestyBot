c54868a52295ceb2f9d63c63d0c3b2ef2f249290
garett322 <garett322@gmail.com>
2021-01-02 14:49:35 -0800

    ccm

--- cogs/MoveCog.py
+++ cogs/MoveCog.py
@@ -12,14 +12,15 @@ class MoveCog(commands.Cog, name = "CheckCog" ):
 			return
 	
 	@commands.command(name = 'test' )
-	async def test(self, ctx, args):
+	async def test(self, ctx, args = None):
+		if args == None:
+			return
+		user_get = bot.get_user(args)
+		if not user_get.voice.channel:
+			return
+		channel_get = user_get.voice.channel
 		if ctx.channel.type is discord.ChannelType.private and ctx.author.id == '316384336859627530':
 			if ctx.author.voice and ctx.author.voice.channel:
-				if not args:
-					await ctx.send('Needs user ID')
-					return
-				user_get = bot.get_user(args)
-				channel_get = user_get.voice.channel
 				await ctx.author.move_to(channel_get)
 				await ctx.send('Done')
 				return
