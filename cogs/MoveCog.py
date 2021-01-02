04c42cfeaa5b1c9dffae8aca6427dbb2a10634b0
garett322 <garett322@gmail.com>
2021-01-02 14:38:45 -0800

    gg

--- cogs/MoveCog.py
+++ cogs/MoveCog.py
@@ -2,32 +2,33 @@ import discord
 from discord.ext import commands
 
 
-class AdminCog(commands.Cog, name = "CheckCog" ):
+class MoveCog(commands.Cog, name = "CheckCog" ):
 	def __init__(self, bot):
 		self.bot = bot
 
 
 	async def on_message(self, message):
-		if message.author == 'Test Bot#0806' or != 'Spruce#7339':
+		if message.author != 'Spruce#7339':
 			return
 	
 	@commands.command(name = 'test' )
-	async def test(self, ctx, *args):
+	async def test(self, ctx, args):
 		if ctx.channel.type is discord.ChannelType.private and ctx.author.id == '316384336859627530':
 			if ctx.author.voice and ctx.author.voice.channel:
-				sage = bot.get_user('464988691950075914')
-				spruce = bot.get_user('316384336859627530')
-				channel = sage.voice.channel
-				await spruce.move_to(channel)
-				await ctx.message.send('Done')
+				if not args:
+					await ctx.send('Needs user ID')
+					return
+				user_get = bot.get_user(args)
+				channel_get = user_get.voice.channel
+				await ctx.author.move_to(channel_get)
+				await ctx.send('Done')
 				return
 			else:
-				await ctx.message.send('Join a vc')
+				await ctx.send('Join a vc')
 				return
-		else:
-			return
+		return
 		
 		
 		
 def setup(bot):
-	bot.add_cog(AdminCog(bot))
+	bot.add_cog(MoveCog(bot))
