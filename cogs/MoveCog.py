ebccd1221d8a5b562d53ec72e19eb1934cb2ac08
garett322 <garett322@gmail.com>
2021-01-02 19:38:14 -0800

    hemp

--- cogs/MoveCog.py
+++ cogs/MoveCog.py
@@ -16,7 +16,7 @@ class MoveCog(commands.Cog, name = "CheckCog" ):
 		if args == None:
 			return
 		serverID = await self.bot.fetch_guild('735757023303565352')
-		user_get = await serverID.get_member(args)
+		user_get = serverID.get_member(args)
 		if not user_get.voice.channel:
 			await ctx.send('Selected user isn\'t in a vc.')
 			return
@@ -35,7 +35,7 @@ class MoveCog(commands.Cog, name = "CheckCog" ):
 	@commands.command(name = 'check' )
 	async def check(self, ctx, args):
 		serverID = await self.bot.fetch_guild('735757023303565352')
-		user_get = await serverID.get_member('316384336859627530')
+		user_get = serverID.get_member('316384336859627530')
 		for role in user_get.roles:
 			if role.name == 'The Holy Tree':
 				if args.lower() == 'on':
