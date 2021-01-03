843ee834a33ccd5b4923122678aa7108759f1d4b
garett322 <garett322@gmail.com>
2021-01-02 19:21:53 -0800

    bbl

--- cogs/MoveCog.py
+++ cogs/MoveCog.py
@@ -8,7 +8,7 @@ class MoveCog(commands.Cog, name = "CheckCog" ):
 
 
 	async def on_message(self, message):
-		if message.author != 'Spruce#7339' or message.channel.type is not discord.ChannelType.private::
+		if message.author != 'Spruce#7339' or message.channel.type is not discord.ChannelType.private:
 			return
 	
 	@commands.command(name = 'test' )
