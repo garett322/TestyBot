79ee688781769a033c6429318de731446c26c0fa
garett322 <garett322@gmail.com>
2021-01-03 18:51:31 -0800

    heck

--- cogs/MuteCog.py
+++ cogs/MuteCog.py
@@ -9,26 +9,35 @@ class SkeletonCog(commands.Cog, name = "SkeletonCog" ):
 		
 	@commands.Cog.listener()
 	async def on_message(self, message):
-		with open('./config/config.json', 'r') as f:
-			config = json.load(f)
-		usermutes = config[ctx.guild.id][mutes][usermute]
-		servermute = config[ctx.guild.id][mutes][servermute]
-		if servermute == 'on':
-			await message.delete()
-			return
-		elif message.author in usermutes:
-			await message.delete()
-			return
+		for role in message.author.roles:
+			if role.name == 'A Fuckin Chad':
+				pass
+		else:
+			with open('./config/config.json', 'r') as f:
+				config = json.load(f)
+			usermutes = config[ctx.guild.id][mutes][usermute]
+			servermute = config[ctx.guild.id][mutes][servermute]
+			rolemutes = config[ctx.guild.id][mutes][rolemute]
+			if servermute == 'on':
+				await message.delete()
+				return
+			elif message.author in usermutes:
+				await message.delete()
+				return
+			for role in message.author.roles:
+				if role.name in rolemutes:
+					await message.delete()
+					return
 
 	@commands.command(name = 'mute' )
-	async def mute(self, ctx, member: discord.member = None):
+	async def mute(self, ctx, choice, muted = None):
 		for role in ctx.author.roles:
 			if role.name == 'A Fuckin Chad':
 				await ctx.delete()
 				with open('./config/config.json', 'r') as f:
 					config = json.load(f)
 					
-				if member == None:
+				if choice == 'all':
 					servermute = config[ctx.guild.id][mutes][servermute]
 					if servermute == 'on':
 						config[ctx.guild.id][mutes][servermute] = 'off'
@@ -39,19 +48,35 @@ class SkeletonCog(commands.Cog, name = "SkeletonCog" ):
 					with open('./config/config.json', 'w') as f:
 						json.dump(config, f, indent = 2)
 					return
-				else:
+				elif choice == 'user':
 					usermutes = config[ctx.guild.id][mutes][usermute]
-					if member in usermutes:
+					if muted in usermutes:
 						usermutes.remove(member)
 						config[ctx.guild.id][mutes][usermute] = usermutes
-						await ctx.author.send('{} has been unmuted'.format(member))
+						await ctx.author.send('{} has been unmuted'.format(muted))
 					else:
 						usermutes.append(member)
 						config[ctx.guild.id][mutes][usermute] = usermutes
-						await ctx.author.send('{} has been muted.'.format(member))
+						await ctx.author.send('{} has been muted.'.format(muted))
 					with open('./config/config.json', 'w') as f:
 						json.dump(usermutes, f, indent = 2)
 					return
-
+				elif choice == 'role':
+					rolemutes = config[ctx.guild.id][mutes][rolemute]
+					if muted in rolemutes:
+						rolemutes.remove(member)
+						config[ctx.guild.id][mutes][rolemute] = rolemutes
+						await ctx.author.send('The {} role has been unmuted'.format(muted))
+					else:
+						rolemutes.append(member)
+						config[ctx.guild.id][mutes][rolemute] = rolemutes
+						await ctx.author.send('The {} role has been muted.'.format(muted))
+					with open('./config/config.json', 'w') as f:
+						json.dump(rolemutes, f, indent = 2)
+					return
+		else:
+			await ctx.send('You don\'t got the perms bruh.')
+			return
+		
 def setup(bot):
 	bot.add_cog(SkeletonCog(bot))
