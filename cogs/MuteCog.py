992cff9649b30caed08582f2c58b7a4b8a895cf0
garett322 <garett322@gmail.com>
2021-01-03 19:43:36 -0800

    help

--- cogs/MuteCog.py
+++ cogs/MuteCog.py
@@ -16,9 +16,9 @@ class MuteCog(commands.Cog, name = "MuteCog" ):
 		else:
 			with open('./config/config.json', 'r') as f:
 				config = json.load(f)
-			usermutes = config[message.guild.id][mutes][usermute]
-			servermute = config[message.guild.id][mutes][servermute]
-			rolemutes = config[message.guild.id][mutes][rolemute]
+			usermutes = config[message.guild.name][mutes][usermute]
+			servermute = config[message.guild.name][mutes][servermute]
+			rolemutes = config[message.guild.name][mutes][rolemute]
 			if servermute == 'on':
 				await message.delete()
 				return
@@ -39,38 +39,38 @@ class MuteCog(commands.Cog, name = "MuteCog" ):
 					config = json.load(f)
 					
 				if choice == 'all':
-					servermute = config[ctx.guild.id][mutes][servermute]
+					servermute = config[ctx.guild.name][mutes][servermute]
 					if servermute == 'on':
-						config[ctx.guild.id][mutes][servermute] = 'off'
+						config[ctx.guild.name][mutes][servermute] = 'off'
 						await ctx.author.send('Server mute removed.')
 					elif servermute == 'off':
-						config[ctx.guild.id][mutes][servermute] = 'on'
+						config[ctx.guild.name][mutes][servermute] = 'on'
 						await ctx.author.send('Server mute applied.')
 					with open('./config/config.json', 'w') as f:
 						json.dump(config, f, indent = 2)
 					return
 				elif choice == 'user':
-					usermutes = config[ctx.guild.id][mutes][usermute]
+					usermutes = config[ctx.guild.name][mutes][usermute]
 					if muted in usermutes:
 						usermutes.remove(member)
-						config[ctx.guild.id][mutes][usermute] = usermutes
+						config[ctx.guild.name][mutes][usermute] = usermutes
 						await ctx.author.send('{} has been unmuted'.format(muted))
 					else:
 						usermutes.append(member)
-						config[ctx.guild.id][mutes][usermute] = usermutes
+						config[ctx.guild.name][mutes][usermute] = usermutes
 						await ctx.author.send('{} has been muted.'.format(muted))
 					with open('./config/config.json', 'w') as f:
 						json.dump(usermutes, f, indent = 2)
 					return
 				elif choice == 'role':
-					rolemutes = config[ctx.guild.id][mutes][rolemute]
+					rolemutes = config[ctx.guild.name][mutes][rolemute]
 					if muted in rolemutes:
 						rolemutes.remove(member)
-						config[ctx.guild.id][mutes][rolemute] = rolemutes
+						config[ctx.guild.name][mutes][rolemute] = rolemutes
 						await ctx.author.send('The {} role has been unmuted'.format(muted))
 					else:
 						rolemutes.append(member)
-						config[ctx.guild.id][mutes][rolemute] = rolemutes
+						config[ctx.guild.name][mutes][rolemute] = rolemutes
 						await ctx.author.send('The {} role has been muted.'.format(muted))
 					with open('./config/config.json', 'w') as f:
 						json.dump(rolemutes, f, indent = 2)
