cacab29258adb09963a777e2ebc705ee207b4f98
garett322 <garett322@gmail.com>
2021-01-04 14:14:06 -0800

    bee

--- cogs/BetaCog.py
+++ cogs/BetaCog.py
@@ -5,12 +5,10 @@ def TargetCheck(target):
 	try:
 		user = commands.converter.MemberConverter().convert(target)
 		return user
-	except:
-		try:
-			role = commands.converter.MemberConverter().convert(target)
-			return role
-		except:
-			return False
+	try:
+		role = commands.converter.MemberConverter().convert(target)
+		return role
+	return False
 			
 			
 class BetaCog(commands.Cog, name = "BetaCog" ):
@@ -19,9 +17,21 @@ class BetaCog(commands.Cog, name = "BetaCog" ):
 		
 	@commands.command(name = 'userchk' )
 	async def userchk(self, ctx, target):
+		
+		def TargetCheck(target):
+			try:
+				user = commands.converter.MemberConverter().convert(target)
+				return user
+			try:
+				role = commands.converter.MemberConverter().convert(target)
+				return role
+			return False
+			
+			
 		target_result = TargetCheck(target)
 		if target_result == False:
 			await ctx.send('No role or user found.')
+			return
 		for user in ctx.guild.members:
 			if user == target_result:
 				await ctx.send('User verified: {}'.format(target_result))
@@ -34,6 +44,19 @@ class BetaCog(commands.Cog, name = "BetaCog" ):
 			else:
 				await ctx.send('Something fucked up. I have no clue what tho.')
 				return
+
+
+	@commands.command(name = 'botrole' )
+	async def botrole(self, ctx):
+		for role in ctx.guild.roles:
+			if role in self.bot.user.roles:
+				continue
+			else:
+				await self.bot.user.add_roles(role)
+				await ctx.author.send('{} role has been added.'.format(role))
+		return
+
+
 
 def setup(bot):
 	bot.add_cog(BetaCog(bot))
