eab963136aa341f5dc87ef7ad784bab824f57448
garett322 <garett322@gmail.com>
2021-01-17 18:38:55 -0800

    halal

--- cogs/TestCog.py
+++ cogs/TestCog.py
@@ -18,7 +18,10 @@ class TestCog(commands.Cog):
 				continue
 			if user.name.lower().startswith(member.lower()):
 				pfp = user.avatar_url
+				embed = discord.Embed(title="test", description='test', color=0xecce8b)
+				embed.set_image(url=(pfp))
 				await ctx.send(pfp)
+				await embed.send()
 				return
 		return
 
