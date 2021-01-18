86c078af4015a934682047da45415f60aee224b9
garett322 <garett322@gmail.com>
2021-01-17 18:27:39 -0800

    ball

--- cogs/TestCog.py
+++ cogs/TestCog.py
@@ -10,6 +10,19 @@ class TestCog(commands.Cog):
 	async def testy(self, ctx):
 		await ctx.send('It Works!')
 		return
+	
+	@commands.command()
+	async def avatar(self, ctx):
+		for user in ctx.guild.members:
+			if user.id == 684444511861997680 or not user.voice:
+				continue
+			if user.name.lower().startswith(member.lower()):
+				pfp = user.avatar_url
+    		embed = discord.Embed(title="test", description='test', color=0xecce8b)
+    		embed.set_image(url=(pfp))
+    		await ctx.send(embed)
+    		return
+		return
 
 def setup(bot):
 	bot.add_cog(TestCog(bot))
