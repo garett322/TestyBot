a8906c2f31953772b41c492a5bfc33e752f8f6c7
garett322 <garett322@gmail.com>
2021-01-19 18:38:24 -0800

    just

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def on_message(message):
 		if message.channel.name == 'bot-commands' or message.channel.name == 'bot-commands-beta' or message.author.id == '316384336859627530':
 			pass
 		else:
-			await ctx.send('316384336859627530, ' + message.author.id)
+			await message.channel.send('316384336859627530, ' + message.author.id)
 			await message.author.send('Please use the bot-commands channel. Thanks!')
 			return
 	if message.content.startswith('.'):
