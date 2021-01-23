6d279782b3024471d321d2359e9f4d104ce9653f
garett322 <garett322@gmail.com>
2021-01-22 17:01:12 -0800

    just

--- cogs/ImageCog.py
+++ cogs/ImageCog.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 import PIL
 from PIL import Image, ImageFont, ImageDraw
 import tempfile
-import urllib2
-from urllib.parse import urlparse
 import requests
 import shutil
 
@@ -14,37 +12,47 @@ class ImageCog(commands.Cog, name = "ImageCog"):
 		self.bot = bot
 		
 	@ commands.command(name = 'image')
-	async def image(self, ctx, url, x_coord, y_coord, size = 100, * , text):
-		try:
-			urllib2.urlopen(url)
-		except urllib2.HTTPError:
+	async def image(self, ctx, url = None, x_coord = 0, y_coord = 0, size = 1, * , text = ':)'):
+		if url = None:
+			await ctx send('You need to give me the link to an image you want to use.')
+		file = requests.get(url, stream = True)
+		if file.status_code == 200:
+			pass
+		elif file.status_code == 400:
 			await ctx.send('I couldn\'t find that url. Please try again.')
-		except urllib2.URLError:
-			await ctx.send('I couldn\'t find that url. Please try again.')
-		file = requests.get(url)
-		parsed_url = urlparse(url)
-		filename = parsed_url.rsplit('/')[-1]
-		filetype = filename.rsplit('.')[-1]
-		original = tempfile.NamedTemporaryFile(suffix = '.{}'.format(filetype))
+			return
+		else:
+			await ctx.send('An unknown error has occurred.')
+			return
+		
+		image_formats = ("image/png", "image/jpeg", "image/jpg")
+		if file.headers['content-type'] not in image_formats:
+			await ctx.send('The url you gave is not an image.')
+			return
+		else:
+			filetype = '.' + file.headers['content-type'].slice(6, 11).strip(';')
+		
+		original = tempfile.NamedTemporaryFile(suffix = filetype)
 		with open(original, 'wb') as f:
+			file.raw.decode_content = True
 			shutil.copyfileobj(file.raw, f)
+
 		im = Image.open(original)
 		draw = ImageDraw.Draw(im)
 		font = ImageFont.truetype('./fonts/news-cycle.ttf', int(size))
 		draw.text((int(x_coord), int(y_coord)), text, font = font, fill = 'black')
-		if filetype.lower() == 'jpg':
-			edited = tempfile.NamedTemporaryFile(suffix = '.jpg')
-			im.save(edited, 'JPEG')
-		elif filetype.lower() == 'png':
-			edited = tempfile.NamedTemporaryFile(suffix = '.png')
-			im.save(edited, 'PNG')
-		else:
-			await ctx.send('I don\'t recognize that file type. Please try a different picture.')
-			return
-		file = discord.File(edited.name, filename = 'image.jpg')
+		
+		edited = tempfile.NamedTemporaryFile(suffix = filetype)
+		if filetype == '.jpg':
+			filetype = '.jpeg'
+		filetype = filetype.strip('.')
+		im.save(edited, filetype)
+		
+		final_file = discord.File(edited.name, filename = 'image.jpg')
 		embed = discord.Embed(color = discord.Colour.green())
 		embed.set_image(url = 'attachment://image.jpg')
-		await ctx.send(file = file, embed = embed)
+		await ctx.send(file = final_file, embed = embed)
+		
 		original.close()
 		edited.close()
 		return
