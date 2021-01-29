b7cdfd62d19fbf1cab8620bee7601f6d5bd86d25
garett322 <garett322@gmail.com>
2021-01-29 13:47:42 -0800

    huffy

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix = ".", intents = intents)
 
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 
 @client.event
@@ -13,13 +13,13 @@ async def on_ready():
 	user = await client.fetch_user('316384336859627530')
 	await user.send('Bot is ready to go!')
 	
-	#logger = logging.getLogger('discord')
-	#logger.setLevel(logging.DEBUG)
-	#handler = logging.StreamHandler()
-	#handler.setLevel(logging.WARNING)
-	#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s - %(lineno)d', datefmt='%m/%d/%Y %I:%M:%S %p')
-	#handler.setFormatter(formatter)
-	#logger.addHandler(handler)
+	logger = logging.getLogger('discord.client')
+	logger.setLevel(logging.DEBUG)
+	handler = logging.StreamHandler()
+	handler.setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s - %(lineno)d', datefmt='%m/%d/%Y %I:%M:%S %p')
+	handler.setFormatter(formatter)
+	logger.addHandler(handler)
 	
 @client.event
 async def on_message(message):
@@ -38,7 +38,7 @@ async def on_message(message):
 	
 @client.event
 async def on_command_error(ctx, error):
-	
+	print(error)
 	return
 	
 
