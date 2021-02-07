9aea9b4dcebbd492fe9b9cfd10eca654d412a43f
garett322 <garett322@gmail.com>
2021-02-06 19:16:14 -0800

    jessi

--- cogs/TriviaCog.py
+++ cogs/TriviaCog.py
@@ -3,10 +3,6 @@ from discord.ext import commands
 import requests
 import random
 
-categories_url = 'https://opentdb.com/api_category.php'
-questions_url = 'https://opentdb.com/api.php?amount={}&category={}&difficulty={}'
-
-
 
 
 class TriviaCog(commands.Cog, name = 'Trivia'):
@@ -16,12 +12,12 @@ class TriviaCog(commands.Cog, name = 'Trivia'):
 	@commands.group(name = 'trivia', description = 'Trivia command')
 	async def trivia(self, ctx):
 		if ctx.invoked_subcommand is None:
-			await ctx.send('The available options for trivia are: start.')
+			await ctx.send('The available options for trivia are: "start" and "categories"')
 			return
 		
 		
 	@trivia.command(name = 'start', description = 'Starts your trivia game')
-	async def start(self, ctx, difficulty = None, questions = 5, category = None):
+	async def start(self, ctx, difficulty = None, *, category = None):
 		
 		if difficulty == None:
 			difficulty = ''
@@ -31,7 +27,7 @@ class TriviaCog(commands.Cog, name = 'Trivia'):
 		if category == None:
 			category = ''
 		else:
-			categories_json = requests.get(categories_url).json()
+			categories_json = requests.get('https://opentdb.com/api_category.php').json()
 			for category_chk in categories_json['trivia_categories']:
 				if category_chk['name'].startswith('Entertainment:'):
 					x = slice(15, None, None)
@@ -46,117 +42,93 @@ class TriviaCog(commands.Cog, name = 'Trivia'):
 					category = f"&category={category_chk['id']}"
 					break
 			else:
-				await ctx.send(f'I couldn\'t find a category called "{category}". Use the command ".trivia categories" to get a list of available categories.')
+				await ctx.send(f'I couldn\'t find a category called "{category}". Use the command ".trivia categories" to get a list of all available categories.')
 		
-		try:
-			questions = int(questions)
-		except:
-			await ctx.send(f'{questions} is not a valid number. Please try again.')
-			return
+		
 			
-		questions_json = requests.get(f'https://opentdb.com/api.php?amount={questions}{category}{difficulty}').json()
-		if questions_json['response_code'] == 0:
+		question_json = requests.get(f'https://opentdb.com/api.php?amount=1{category}{difficulty}').json()
+		if question_json['response_code'] == 0:
 			pass
-		elif questions_json['response_code'] == 1:
-			await ctx.send(f'I couldn\'t find {questions} questions. Please try a smaller number.')
-			return
-		elif questions_json['response_code'] == 2:
+		elif question_json['response_code'] == 2:
 			await ctx.send('An argument you gave caused an error. Please try again.')
 			return
 		else:
 			await ctx.send('Unknown error.')
 			return
-		counter = 1
-		fails = 0
-		for question in questions_json['results']:
+		question = question_json['results']
+		if question['category'].startswith('Entertainment:'):
+			x = slice(15, None, None)
+			category = str(question['category'])[x]
+		elif question['category'].startswith('Science:'):
+			x = slice(9, None, None)
+			category = str(question['category'])[x]
+		else:
+			category = str(question['category'])
 			
-			if question['category'].startswith('Entertainment:'):
-				x = slice(15, None, None)
-				category = str(question['category'])[x]
-			elif question['category'].startswith('Science:'):
-				x = slice(9, None, None)
-				category = str(question['category'])[x]
-			else:
-				category = str(question['category'])
+		difficulty = str(question['difficulty'])[0].upper() + str(question['difficulty'])[1:]
+			
+		if question['type'] == 'boolean':
+			answer_place = random.randint(1,2)
+			embed = discord.Embed(title = question['question'])
+			embed.add_field(name = 'Answers:', value = 'True\nFalse')
+			embed.set_footer(text = f"Category: {category}; Difficulty: {difficulty}")
+			message = await ctx.send(embed = embed)
+			await message.add_reaction('☑️')
+			await message.add_reaction('❎')
+			qtype = 1
 				
-			difficulty = str(question['difficulty'])[0].upper() + str(question['difficulty'])[1:]
-				
-			if question['type'] == 'boolean':
-				answer_place = random.randint(1,2)
-				embed = discord.Embed(title = question['question'], description = f'Question {counter} of {questions}')
-				embed.add_field(name = 'Answers:', value = 'True\nFalse')
-				embed.set_footer(text = f"Category: {category}; Difficulty: {difficulty}")
-				message = await ctx.send(embed = embed)
-				await message.add_reaction('☑️')
-				await message.add_reaction('❎')
-				qtype = 1
-				
-			else:
-				answer_place = random.randint(0,3)
-				wrong_answer_list = question['incorrect_answers']
-				embed = discord.Embed(title = question['question'], description = f'Question {counter} of {questions}')
-				answer_list = ''
-				x = 0
-				i = 0
-				while i <= 3:
-					if i == answer_place:
-						answer_list = answer_list + question['correct_answer'] + '\n'
-					else:
-						answer_list = answer_list + wrong_answer_list[i] + '\n'
-						x = x + 1
-					i = i + 1
-				embed.add_field(name = 'Answers:', value = answer_list)
-				embed.set_footer(text = f"Category: {category}; Difficulty: {difficulty}")
-				message = await ctx.send(embed = embed)
-				await message.add_reaction('1️⃣')
-				await message.add_reaction('2️⃣')
-				await message.add_reaction('3️⃣')
-				await message.add_reaction('4️⃣')
-				qtype = 2
-				
-			emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣']
-			mc_answer_emojis = {'1️⃣': 0, '2️⃣': 1, '3️⃣': 2, '4️⃣': 3}
-			tf_answer_emojis = {'☑️': 0, '❎': 1}
-			def check(reaction, user):
-				if user == ctx.author:
-					if qtype == 1:
-						return str(reaction.emoji) in tf_answer_emojis or str(reaction.emoji) == '⛔'
-					elif qtype == 2:
-						return str(reaction.emoji) in mc_answer_emojis or str(reaction.emoji) == '⛔'
-					
-			try:
-				reaction, user = await client.wait_for('reaction_add', timeout=20.0, check=check)
-			except asyncio.TimeoutError:
-				fails = fails + 1
-				if fails == 3:
-					await ctx.send('You ran out of time 3 times in a row. Trivia has been automatically cancelled.')
-					await message.delete()
-					return
+		else:
+			answer_place = random.randint(0,3)
+			wrong_answer_list = question['incorrect_answers']
+			embed = discord.Embed(title = question['question'])
+			answer_list = ''
+			x = 0
+			i = 0
+			while i <= 3:
+				if i == answer_place:
+					answer_list = answer_list + question['correct_answer'] + '\n'
 				else:
-					await ctx.send('You ran out of time to answer. Next question.')
-			else:
-				fails = 0
+					answer_list = answer_list + wrong_answer_list[i] + '\n'
+					x = x + 1
+				i = i + 1
+			embed.add_field(name = 'Answers:', value = answer_list)
+			embed.set_footer(text = f"Category: {category}; Difficulty: {difficulty}")
+			message = await ctx.send(embed = embed)
+			await message.add_reaction('1️⃣')
+			await message.add_reaction('2️⃣')
+			await message.add_reaction('3️⃣')
+			await message.add_reaction('4️⃣')
+			qtype = 2
+			
+		emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣']
+		mc_answer_emojis = {'1️⃣': 0, '2️⃣': 1, '3️⃣': 2, '4️⃣': 3}
+		tf_answer_emojis = {'☑️': 0, '❎': 1}
+		
+		def check(reaction, user):
+			if user == ctx.author:
 				if qtype == 1:
-					answer_emojis = tf_answer_emojis
+					return str(reaction.emoji) in tf_answer_emojis
 				elif qtype == 2:
-					answer_emojis = tf_answer_emojis
+					return str(reaction.emoji) in mc_answer_emojis
 					
-				user_answer = str(reaction.emoji)
-				if answer_emojis[user_answer] == answer_place:
-					await ctx.send('CORRECT!')
-					await ctx.send('Next question...')
-				elif user_answer == '⛔':
-					await ctx.send('Trivia cancelled.')
-					await message.delete()
-					return
-				else:
-					await ctx.send('INCORRECT.')
-					await ctx.send(f"The correct answer was: {question['correct_answer']}.")
-					await ctx.send('Next question...'
-					
-			await asyncio.sleep(2)
-			await message.delete()
-			counter = counter + 1
+		try:
+			reaction, user = await client.wait_for('reaction_add', timeout=20.0, check=check)
+		except asyncio.TimeoutError:
+			await ctx.send('You ran out of time to answer. Next question.')
+			return
+		else:
+			fails = 0
+			if qtype == 1:
+				answer_emojis = tf_answer_emojis
+			elif qtype == 2:
+				answer_emojis = tf_answer_emojis
+				
+			user_answer = str(reaction.emoji)
+			if answer_emojis[user_answer] == answer_place:
+				await ctx.send(f'CORRECT! Good job {user.name}!')
+			else:
+				await ctx.send('INCORRECT.')
+				await ctx.send(f"The correct answer was: {question['correct_answer']}.")
 
 
 	@trivia.command(name = 'categories', description = 'Shows all available catagories.')
