9b71b0547d6ef3fbd4ae77ef6f6c39b64703282a
garett322 <garett322@gmail.com>
2021-02-08 17:18:16 -0800

    next

--- cogs/TriviaCog.py
+++ cogs/TriviaCog.py
@@ -173,10 +173,10 @@ class TriviaCog(commands.Cog, name = 'Trivia'):
 		cheaters = str(cheater_list).strip('][').replace("'", '')
 		
 		embed = discord.Embed(title = 'Results!', description = question_str)
-		embed.add_field(name = 'Correct answer:', value = answer_str + '\u200b')
-		embed.add_field(name = 'Who got it right:', value = correct_users + '\u200b')
-		embed.add_field(name = 'Who got it wrong:', value = incorrect_users + '\u200b')
-		embed.add_field(name = 'Cheaters:', value = cheaters + '\u200b')
+		embed.add_field(name = 'Correct answer:', value = answer_str)
+		embed.add_field(name = 'Who got it right:', value = correct_users)
+		embed.add_field(name = 'Who got it wrong:', value = incorrect_users)
+		embed.add_field(name = 'Cheaters:', value = cheaters)
 		await question_embed.delete()
 		await ctx.send(embed = embed)
 
