48f0fd526aa392b713a40ef3b5ada5984bec84be
garett322 <garett322@gmail.com>
2021-05-25 11:05:51 -0700

    Yeezy

--- cogs/SkeletonCog.py
+++ cogs/SkeletonCog.py
@@ -1,16 +1,18 @@
 import discord
 from discord.ext import commands
+import aiohttp
 
 
-class SkeletonCog(commands.Cog, name = "SkeletonCog" ):
+
+class COG_NAME_MUST_CHANGE(commands.Cog, name = "COG_NAME_MUST_CHANGE" ):
 	def __init__(self, bot):
 		self.bot = bot
 		
-	@commands.command(name = 'example' )
+	@commands.command(name = 'COMMAND_NAME_MUST_CHANGE' )
 	async def example(self, ctx):
 		return
       
         
 
 def setup(bot):
-	bot.add_cog(SkeletonCog(bot))
+	bot.add_cog(COG_NAME_MUST_CHANGE(bot))
