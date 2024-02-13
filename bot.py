5e2e3b017a4f811c1eeea8026b23afb4157ab3e2
LordSunday1 <159904039+LordSunday1@users.noreply.github.com>
2024-02-13 06:10:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,19 +9,14 @@ client = commands.Bot(command_prefix = ".", intents = intents)
 @client.event
 async def on_ready():
 	print('Logged in as {}'.format(client.user))
-	user = await client.fetch_user('316384336859627530')
-	await user.send('Bot is ready to go!')
 	
 @client.event
 async def on_message(message):
 	if message.author is client.user:
 		return
-	elif message.content.lower() == '!pv':
-	  await message.channel.send('Thanks for the positive vibes {}!!'.format(message.author.nick))
-	  return
 	elif not message.content.startswith('.'):
 		return
-	elif message.channel.name == 'bot-commands' or message.channel.name == 'bot-commands-beta' or message.channel.name == 'spruce-bot-commands':
+	elif message.channel.name == 'bot-commands':
 		pass
 	else:
 		return
@@ -30,11 +25,8 @@ async def on_message(message):
 
 
 
-client.load_extension('cogs.ImageCog')
 client.load_extension('cogs.TestCog')
-#client.load_extension('cogs.RoleCog')
-#client.load_extension('cogs.VcCog')
-#client.load_extension('cogs.ErrorCog')
+client.load_extension('cogs.ErrorCog')
 client.load_extension('cogs.TriviaCog')
 client.load_extension('cogs.UnloadCog')
 
